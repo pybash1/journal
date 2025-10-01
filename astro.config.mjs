f95718cec5759ff121c524f5a2ebaae2a0b73a89
PyBash <67195650+pybash1@users.noreply.github.com>
2025-10-01 17:04:17 +0530

    updated and simplified design and ui

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
+import remarkGithubAlerts from "remark-github-alerts";
 
 import react from "@astrojs/react";
 import tailwindcss from "@tailwindcss/vite";
@@ -9,8 +10,16 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
   site: "https://journal.pybash.xyz",
-  integrations: [mdx({ gfm: true }), sitemap(), react()],
-
+  integrations: [mdx({ 
+    gfm: true,
+    remarkPlugins: [remarkGithubAlerts]
+  }), sitemap(), react()],
+  markdown: {
+    remarkPlugins: [remarkGithubAlerts],
+    shikiConfig: {
+      theme: "vitesse-dark",
+    },
+  },
   vite: {
     plugins: [tailwindcss()],
   },
